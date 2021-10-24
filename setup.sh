26e213d6089aa18b8f035ac39ae274df721f2fb5
nikiandr <nikita.forduy@protonmail.com>
2021-10-24 21:27:25 +0300

    added files to deploy

---
+++ setup.sh
@@ -0,0 +1,13 @@
+apt-get update -y
+apt install -y chromium-driver
+mkdir -p ~/.streamlit/
+echo "\
+[theme]\n\
+base="light"\n\
+primaryColor=\"#20beff\"\n\
+[server]\n\
+headless = true\n\
+port = $PORT\n\
+enableCORS = false\n\
+\n\
+" > ~/.streamlit/config.toml
