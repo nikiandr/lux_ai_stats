e2fc4559da1fa619ea023af2f00950b0c8f5dd64
nikiandr <nikita.forduy@protonmail.com>
2021-10-24 22:08:49 +0300

    try #83921892139

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
 mkdir -p ~/.streamlit/
+apt-get install -y libglib2.0-0=2.50.3-2 \
+    libnss3=2:3.26.2-1.1+deb9u1 \
+    libgconf-2-4=3.2.6-4+b1 \
+    libfontconfig1=2.11.0-6.7+b1
 echo "\
 [theme]\n\
 base=\"light\"\n\
