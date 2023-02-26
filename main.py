214034afb4eb512ab66caf7a4be8242c67609fc1
Andrii Yerko <aerko12@gmail.com>
2023-02-26 12:51:55 +0200

    style: format code

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
-import streamlit as st
 import numpy as np
 import plotly.graph_objs as go
-import plotly.express as px
-from tools import getSoup, getStats, ewma
+import streamlit as st
 
-st.set_page_config(page_title='Lux AI stats', 
-                   page_icon='🤖',
-                   layout='wide',
-                   menu_items={
-                       'About': 'Submission statistics for Lux AI competition. Made by Yalikesifulei from Team ♂ GARCH ♂.'
-                   })
+from tools import ewma, getSoup, getStats
+
+st.set_page_config(
+    page_title="Lux AI stats",
+    page_icon="🤖",
+    layout="wide",
+    menu_items={"About": "Submission statistics for Lux AI competition. Made by Yalikesifulei from Team ♂ GARCH ♂."},
+)
 
 hide_menu_style = """
         <style>
         #MainMenu {visibility: hidden;}
         footer:after {
-            content: 'Submission statistics for Lux AI competition. Made by Yalikesifulei from Team ♂ GARCH ♂.'; 
+            content: 'Submission statistics for Lux AI competition. Made by Yalikesifulei from Team ♂ GARCH ♂.';
             visibility: visible;
             display: block;
             position: relative;
@@ -25,13 +25,13 @@ hide_menu_style = """
         """
 st.markdown(hide_menu_style, unsafe_allow_html=True)
 
-st.title('Lux AI submission statistics')
+st.title("Lux AI submission statistics")
 col1, col2 = st.columns(2)
 
-sub_id = col1.number_input('Submission ID', value=30131764, step=1, key='sub_id')
+sub_id = col1.number_input("Submission ID", value=30131764, step=1, key="sub_id")
 
-if col2.button('Get stats', key='run'):
-    with st.spinner('Wait for it...'):
+if col2.button("Get stats", key="run"):
+    with st.spinner("Wait for it..."):
         try:
             soup = getSoup(sub_id)
             team_name, scores, outcomes, scores_delta, hover_text = getStats(soup)
@@ -39,217 +39,172 @@ if col2.button('Get stats', key='run'):
         except ValueError as e:
             print(e)
             is_ok = False
-            st.error('**Error:** wrong submission ID!')
+            st.error("**Error:** wrong submission ID!")
 
     if is_ok:
         info_cols = st.columns(3)
         info_cols[0].subheader(team_name)
-        info_cols[1].subheader(f'Current score: {scores[-1]}')
-        info_cols[2].subheader(f'Current win rate: {np.mean(outcomes):.3f}')
+        info_cols[1].subheader(f"Current score: {scores[-1]}")
+        info_cols[2].subheader(f"Current win rate: {np.mean(outcomes):.3f}")
 
         plot_cols = st.columns(2)
-        layout = go.Layout(
-            paper_bgcolor='rgba(0,0,0,0)',
-            plot_bgcolor='rgba(0,0,0,0)'
-        )
-        gray_color = 'rgba(49,51,63,1)'
-        gray_color_tr = 'rgba(49,51,63,0.5)'
+        layout = go.Layout(paper_bgcolor="rgba(0,0,0,0)", plot_bgcolor="rgba(0,0,0,0)")
+        gray_color = "rgba(49,51,63,1)"
+        gray_color_tr = "rgba(49,51,63,0.5)"
 
-        plot_cols[0].write('#### Score growth plot')
-        
+        plot_cols[0].write("#### Score growth plot")
+
         fig = go.Figure(layout=layout)
-        fig.add_trace(go.Scatter(
-            x=np.arange(1, len(scores)+1), y=scores,
-            name='scores',
-            line={'color': '#1f77b4'}
-        ))
-        fig.add_trace(go.Scatter(
-            mode='lines',
-            x=[1, len(scores)+1], y=[np.mean(scores)] * 2,
-            name=f'mean score {np.mean(scores):.2f}',
-            line={'color':'lightsalmon'}
-        ))
-        fig.add_trace(go.Scatter(
-            mode='lines',
-            x=[1, len(scores)+1], y=[np.median(scores)] * 2,
-            name=f'median score {np.median(scores):.2f}',
-            line={'color':'lightseagreen'}
-        ))
-        fig.add_trace(go.Scatter(
-            mode='markers',
-            x=[np.argmax(scores)+1], y=[np.max(scores)],
-            marker={
-                'color': 'mediumseagreen',
-                'size': 10,
-                'symbol': 'circle',
-                'line': {'color': gray_color, 'width': 1}
-            },
-            name=f'top score {np.max(scores)}'
-        ))
-        fig.update_layout(
-            showlegend=True,
-            xaxis_title='Match number',
-            margin=dict(l=0, r=0, b=0, t=0))
+        fig.add_trace(go.Scatter(x=np.arange(1, len(scores) + 1), y=scores, name="scores", line={"color": "#1f77b4"}))
+        fig.add_trace(
+            go.Scatter(
+                mode="lines",
+                x=[1, len(scores) + 1],
+                y=[np.mean(scores)] * 2,
+                name=f"mean score {np.mean(scores):.2f}",
+                line={"color": "lightsalmon"},
+            )
+        )
+        fig.add_trace(
+            go.Scatter(
+                mode="lines",
+                x=[1, len(scores) + 1],
+                y=[np.median(scores)] * 2,
+                name=f"median score {np.median(scores):.2f}",
+                line={"color": "lightseagreen"},
+            )
+        )
+        fig.add_trace(
+            go.Scatter(
+                mode="markers",
+                x=[np.argmax(scores) + 1],
+                y=[np.max(scores)],
+                marker={"color": "mediumseagreen", "size": 10, "symbol": "circle", "line": {"color": gray_color, "width": 1}},
+                name=f"top score {np.max(scores)}",
+            )
+        )
+        fig.update_layout(showlegend=True, xaxis_title="Match number", margin=dict(l=0, r=0, b=0, t=0))
         fig.update_xaxes(showline=True, linecolor=gray_color, gridcolor=gray_color_tr, zerolinecolor=gray_color_tr)
         fig.update_yaxes(showline=True, linecolor=gray_color, gridcolor=gray_color_tr)
 
-        plot_cols[0].plotly_chart(fig, use_container_width=True, config={'displayModeBar': False})
+        plot_cols[0].plotly_chart(fig, use_container_width=True, config={"displayModeBar": False})
 
-
-        plot_cols[1].write(f'#### Win/Loss/Tie plot by match')
+        plot_cols[1].write("#### Win/Loss/Tie plot by match")
         fig = go.Figure(layout=layout)
-        fig.add_trace(go.Scatter(
-            x=np.arange(1, len(outcomes)+1), y=outcomes,
-            line={'color': 'lightgray', 'dash': 'dash'},
-            showlegend=False
-        ))
-        fig.add_trace(go.Scatter(
-            mode='markers',
-            x=np.argwhere(outcomes == 1).flatten()+1, y=outcomes[outcomes == 1],
-            text=hover_text[outcomes == 1],
-            hoverinfo='text',
-            marker={
-                'color': 'mediumseagreen',
-                'size': 10,
-                'symbol': 'circle',
-                'line': {'color': gray_color, 'width': 1}
-            },
-            name=f'Win'
-        ))
-        fig.add_trace(go.Scatter(
-            mode='markers',
-            x=np.argwhere(outcomes == 0).flatten()+1, y=outcomes[outcomes == 0],
-            text=hover_text[outcomes == 0],
-            hoverinfo='text',
-            marker={
-                'color': 'tomato',
-                'size': 10,
-                'symbol': 'circle',
-                'line': {'color': gray_color, 'width': 1}
-            },
-            name=f'Loss'
-        ))
-        fig.add_trace(go.Scatter(
-            mode='markers',
-            x=np.argwhere(outcomes == 0.5).flatten()+1, y=outcomes[outcomes == 0.5],
-            text=hover_text[outcomes == 0.5],
-            hoverinfo='text',
-            marker={
-                'color': 'deepskyblue',
-                'size': 10,
-                'symbol': 'circle',
-                'line': {'color': gray_color, 'width': 1}
-            },
-            name=f'Tie'
-        ))
-        fig.update_layout(
-            showlegend=True,
-            xaxis_title='Match number',
-            margin=dict(l=0, r=0, b=0, t=0))
+        fig.add_trace(
+            go.Scatter(x=np.arange(1, len(outcomes) + 1), y=outcomes, line={"color": "lightgray", "dash": "dash"}, showlegend=False)
+        )
+        fig.add_trace(
+            go.Scatter(
+                mode="markers",
+                x=np.argwhere(outcomes == 1).flatten() + 1,
+                y=outcomes[outcomes == 1],
+                text=hover_text[outcomes == 1],
+                hoverinfo="text",
+                marker={"color": "mediumseagreen", "size": 10, "symbol": "circle", "line": {"color": gray_color, "width": 1}},
+                name="Win",
+            )
+        )
+        fig.add_trace(
+            go.Scatter(
+                mode="markers",
+                x=np.argwhere(outcomes == 0).flatten() + 1,
+                y=outcomes[outcomes == 0],
+                text=hover_text[outcomes == 0],
+                hoverinfo="text",
+                marker={"color": "tomato", "size": 10, "symbol": "circle", "line": {"color": gray_color, "width": 1}},
+                name="Loss",
+            )
+        )
+        fig.add_trace(
+            go.Scatter(
+                mode="markers",
+                x=np.argwhere(outcomes == 0.5).flatten() + 1,
+                y=outcomes[outcomes == 0.5],
+                text=hover_text[outcomes == 0.5],
+                hoverinfo="text",
+                marker={"color": "deepskyblue", "size": 10, "symbol": "circle", "line": {"color": gray_color, "width": 1}},
+                name="Tie",
+            )
+        )
+        fig.update_layout(showlegend=True, xaxis_title="Match number", margin=dict(l=0, r=0, b=0, t=0))
         fig.update_xaxes(showline=True, linecolor=gray_color, gridcolor=gray_color_tr, zerolinecolor=gray_color_tr)
         fig.update_yaxes(showline=True, linecolor=gray_color, gridcolor=gray_color_tr, zerolinecolor=gray_color_tr)
 
-        plot_cols[1].plotly_chart(fig, use_container_width=True, config={'displayModeBar': False})
+        plot_cols[1].plotly_chart(fig, use_container_width=True, config={"displayModeBar": False})
 
-
-        plot_cols[0].write('#### Score changes (delta) plot')
+        plot_cols[0].write("#### Score changes (delta) plot")
         fig = go.Figure(layout=layout)
-        fig.add_trace(go.Scatter(
-            x=np.arange(1, len(scores_delta)+1), y=scores_delta,
-            showlegend=False,
-            line={'color': '#1f77b4'}
-        ))
-        fig.add_trace(go.Scatter(
-            mode='markers',
-            x=np.argwhere(scores_delta > 0).flatten()+1, y=scores_delta[scores_delta > 0],
-            text=hover_text[scores_delta > 0],
-            hoverinfo='text',
-            marker={
-                'color': 'mediumseagreen',
-                'size': 10,
-                'symbol': 'circle',
-                'line': {'color': gray_color, 'width': 1}
-            },
-            name=f'Positive'
-        ))
-        fig.add_trace(go.Scatter(
-            mode='markers',
-            x=np.argwhere(scores_delta < 0).flatten()+1, y=scores_delta[scores_delta < 0],
-            text=hover_text[scores_delta < 0],
-            hoverinfo='text',
-            marker={
-                'color': 'tomato',
-                'size': 10,
-                'symbol': 'circle',
-                'line': {'color': gray_color, 'width': 1}
-            },
-            name=f'Negative'
-        ))
-        fig.update_layout(
-            showlegend=True, 
-            xaxis_title='Match number',
-            margin=dict(l=0, r=0, b=0, t=0))
+        fig.add_trace(go.Scatter(x=np.arange(1, len(scores_delta) + 1), y=scores_delta, showlegend=False, line={"color": "#1f77b4"}))
+        fig.add_trace(
+            go.Scatter(
+                mode="markers",
+                x=np.argwhere(scores_delta > 0).flatten() + 1,
+                y=scores_delta[scores_delta > 0],
+                text=hover_text[scores_delta > 0],
+                hoverinfo="text",
+                marker={"color": "mediumseagreen", "size": 10, "symbol": "circle", "line": {"color": gray_color, "width": 1}},
+                name="Positive",
+            )
+        )
+        fig.add_trace(
+            go.Scatter(
+                mode="markers",
+                x=np.argwhere(scores_delta < 0).flatten() + 1,
+                y=scores_delta[scores_delta < 0],
+                text=hover_text[scores_delta < 0],
+                hoverinfo="text",
+                marker={"color": "tomato", "size": 10, "symbol": "circle", "line": {"color": gray_color, "width": 1}},
+                name="Negative",
+            )
+        )
+        fig.update_layout(showlegend=True, xaxis_title="Match number", margin=dict(l=0, r=0, b=0, t=0))
         fig.update_xaxes(showline=True, linecolor=gray_color, gridcolor=gray_color_tr, zerolinecolor=gray_color_tr)
-        fig.update_yaxes(showline=True, linecolor=gray_color, gridcolor=gray_color_tr, zerolinecolor='black')
+        fig.update_yaxes(showline=True, linecolor=gray_color, gridcolor=gray_color_tr, zerolinecolor="black")
 
-        plot_cols[0].plotly_chart(fig, use_container_width=True, config={'displayModeBar': False})
+        plot_cols[0].plotly_chart(fig, use_container_width=True, config={"displayModeBar": False})
 
-
-        plot_cols[1].write('#### Win rate change by match')
+        plot_cols[1].write("#### Win rate change by match")
         alpha_values = np.linspace(0.0, 0.1, 21)[1:]
         fig = go.Figure(layout=layout)
-        wr_change = [sum(outcomes[:n])/n for n in range(1, len(outcomes)+1)]
+        wr_change = [sum(outcomes[:n]) / n for n in range(1, len(outcomes) + 1)]
 
         for alpha in alpha_values:
-            fig.add_trace(go.Scatter(
-                visible=False,
-                name=f'win rate EMA',
-                x=np.arange(1, len(outcomes)+1),
-                y=ewma(wr_change, alpha=alpha),
-                line={'color': '#2ca02c'}
-            ))
-        fig.data[len(alpha_values)//2 - 1].visible = True
+            fig.add_trace(
+                go.Scatter(
+                    visible=False,
+                    name="win rate EMA",
+                    x=np.arange(1, len(outcomes) + 1),
+                    y=ewma(wr_change, alpha=alpha),
+                    line={"color": "#2ca02c"},
+                )
+            )
+        fig.data[len(alpha_values) // 2 - 1].visible = True
 
-        fig.add_trace(go.Scatter(
-            x=np.arange(1, len(scores_delta)+1), y=wr_change,
-            name='win rate',
-            line={'color': '#1f77b4'}
-        ))
+        fig.add_trace(go.Scatter(x=np.arange(1, len(scores_delta) + 1), y=wr_change, name="win rate", line={"color": "#1f77b4"}))
         fig.data[-1].visible = True
 
-        fig.add_trace(go.Scatter(
-            mode='lines',
-            x=[1, len(scores)+1], y=[np.mean(outcomes)] * 2,
-            name=f'current winrate',
-            line={'color':'lightsalmon'}
-        ))
+        fig.add_trace(
+            go.Scatter(
+                mode="lines", x=[1, len(scores) + 1], y=[np.mean(outcomes)] * 2, name="current winrate", line={"color": "lightsalmon"}
+            )
+        )
         fig.data[-1].visible = True
 
         steps = []
         for ind, alpha in enumerate(alpha_values):
-            step = {
-                'method': 'update',
-                'args': [{'visible': [False]*len(fig.data)}],
-                'label': f'{alpha}'
-            }
-            step['args'][0]['visible'][ind] = True
-            step['args'][0]['visible'][-1] = True
-            step['args'][0]['visible'][-2] = True
+            step = {"method": "update", "args": [{"visible": [False] * len(fig.data)}], "label": f"{alpha}"}
+            step["args"][0]["visible"][ind] = True
+            step["args"][0]["visible"][-1] = True
+            step["args"][0]["visible"][-2] = True
             steps.append(step)
 
-        sliders = [dict(
-            active=len(alpha_values)//2 - 1,
-            steps=steps,
-            currentvalue={'prefix': 'Smoothing α = '},
-            pad={'b': 3, 't': 40}
-        )]
+        sliders = [dict(active=len(alpha_values) // 2 - 1, steps=steps, currentvalue={"prefix": "Smoothing α = "}, pad={"b": 3, "t": 40})]
 
-        fig.update_layout(
-            showlegend=True,
-            xaxis_title='Match number',
-            margin=dict(l=0, r=0, b=10, t=0),
-            sliders=sliders)
+        fig.update_layout(showlegend=True, xaxis_title="Match number", margin=dict(l=0, r=0, b=10, t=0), sliders=sliders)
         fig.update_xaxes(showline=True, linecolor=gray_color, gridcolor=gray_color_tr, zerolinecolor=gray_color_tr)
-        fig.update_yaxes(showline=True, linecolor=gray_color, gridcolor=gray_color_tr, zerolinecolor=gray_color_tr, range=(min(wr_change)-0.005, 1.005))
+        fig.update_yaxes(
+            showline=True, linecolor=gray_color, gridcolor=gray_color_tr, zerolinecolor=gray_color_tr, range=(min(wr_change) - 0.005, 1.005)
+        )
 
-        plot_cols[1].plotly_chart(fig, use_container_width=True, config={'displayModeBar': False})
+        plot_cols[1].plotly_chart(fig, use_container_width=True, config={"displayModeBar": False})
--- tools.py
+++ tools.py
@@ -1,37 +1,35 @@
-from selenium import webdriver
-from bs4 import BeautifulSoup
-import numpy as np
 import time
+
+import numpy as np
+from bs4 import BeautifulSoup
+from selenium import webdriver
 
 
 def getSoup(sub_id):
     options = webdriver.ChromeOptions()
-    options.add_argument('--headless')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
     browser = webdriver.Chrome(options=options)
 
-    # URL = 'https://www.kaggle.com/c/lux-ai-2021/submissions?dialog=episodes-submission-'
-    URL = 'https://www.kaggle.com/competitions/lux-ai-season-2/leaderboard?dialog=episodes-submission-'
+    URL = "https://www.kaggle.com/competitions/lux-ai-season-2/leaderboard?dialog=episodes-submission-"
 
-    print('Loading submission page...')
+    print("Loading submission page...")
     browser.get(URL + str(sub_id))
     time.sleep(2)
 
-    print('Scrolling results...')
-    scrolling_element = browser.find_element(
-        webdriver.common.by.By.XPATH,
-        "//div[@class='mdc-dialog__surface']")
+    print("Scrolling results...")
+    scrolling_element = browser.find_element(webdriver.common.by.By.XPATH, "//div[@class='mdc-dialog__surface']")
     for k in range(100):
-        browser.execute_script('arguments[0].scrollTop = arguments[0].scrollHeight', scrolling_element)
+        browser.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", scrolling_element)
     time.sleep(1)
 
-    print('Parsing page...')
+    print("Parsing page...")
     html_source = browser.page_source
-    soup = BeautifulSoup(html_source, 'html.parser')
-    print('Done!')
-    
+    soup = BeautifulSoup(html_source, "html.parser")
+    print("Done!")
+
     return soup
 
 
@@ -40,63 +38,49 @@ def getStats(soup):
     scores = []
     scores_delta = []
     hover_text = []
-    links = []
-    
+
     text_select = []
     team_names = []
-    
-    # for a in soup.select('a[class*="sc-"]'):
-    #     try:
-    #         link = a['href']
-    #         if 'submissions?dialog' in link:
-    #             links.append('<a href="https://www.kaggle.com/'+link+'"></a>')
-    #     except KeyError:
-    #         pass
 
     for span in soup.select('span[class*="sc-"]'):
         text = span.get_text()
-        if 'vs' in text and '[' in text and 'ago' not in text:
+        if "vs" in text and "[" in text and "ago" not in text:
             text_select.append(text)
-            for part in text.split(' vs '):
-                part_split = part.split(' ')
-                team_name = ' '.join(part_split[1:-2])
+            for part in text.split(" vs "):
+                part_split = part.split(" ")
+                team_name = " ".join(part_split[1:-2])
                 team_names.append(team_name)
-                
+
     team_name = max(set(team_names), key=team_names.count)
-    
+
     for text in text_select:
-        # print(text)
-        # if 'Validation' not in text and 'Err' not in text:
-        #     hover_text.append(text.replace(' vs ', '<br>'))
-        for part in text.split(' vs '):
+        for part in text.split(" vs "):
             if team_name in part:
-                result = part.split(' ')
-                delta = result[-1].strip('()+')
-                if delta != 'Validation' and delta != '-NaN':
-                    hover_text.append(text.replace(' vs ', '<br>'))
+                result = part.split(" ")
+                delta = result[-1].strip("()+")
+                if delta != "Validation" and delta != "-NaN":
+                    hover_text.append(text.replace(" vs ", "<br>"))
                     scores.append(int(result[-2]))
                     scores_delta.append(int(delta))
-                    outcome = result[0].strip('[]')
-                    if outcome == 'Win':
+                    outcome = result[0].strip("[]")
+                    if outcome == "Win":
                         outcomes.append(1)
-                    elif outcome == 'Loss':
+                    elif outcome == "Loss":
                         outcomes.append(0)
-                    else: # Tie
+                    else:  # Tie
                         outcomes.append(0.5)
-    
 
     scores.insert(0, scores[0] + scores_delta[0])
     scores = np.array(scores[::-1])
     outcomes = np.array(outcomes[::-1])
     scores_delta = np.array(scores_delta[::-1])
     hover_text = np.array(hover_text[::-1])
-    # links = np.array(links[::-1])[1:]
 
-    return team_name, scores, outcomes, scores_delta, hover_text #, links
+    return team_name, scores, outcomes, scores_delta, hover_text
 
 
 def ewma(x, alpha=None):
-    '''
+    """
     Returns the exponentially weighted moving average of x.
 
     Parameters:
@@ -108,7 +92,7 @@ def ewma(x, alpha=None):
     --------
     ewma: numpy array
           the exponentially weighted moving average
-    '''
+    """
     if alpha is None:
         alpha = 2 / (len(x) + 1)
     # Coerce x to an array
@@ -117,11 +101,11 @@ def ewma(x, alpha=None):
 
     # Create an initial weight matrix of (1-alpha), and a matrix of powers
     # to raise the weights by
-    w0 = np.ones(shape=(n,n)) * (1-alpha)
-    p = np.vstack([np.arange(i,i-n,-1) for i in range(n)])
+    w0 = np.ones(shape=(n, n)) * (1 - alpha)
+    p = np.vstack([np.arange(i, i - n, -1) for i in range(n)])
 
     # Create the weight matrix
-    w = np.tril(w0**p,0)
+    w = np.tril(w0**p, 0)
 
     # Calculate the ewma
-    return np.dot(w, x[::np.newaxis]) / w.sum(axis=1)
+    return np.dot(w, x[:: np.newaxis]) / w.sum(axis=1)
